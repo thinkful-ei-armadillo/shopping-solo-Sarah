973f181855541249d202d9bca9438138ff2236e2
Sarah Drew <sarahdrew93@gmail.com>
2019-01-22 07:24:55 -0600

    search feature inmplemented

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const STORE = {
-items : [
-  {name: "apples", checked: false},
-  {name: "oranges", checked: false},
-  {name: "milk", checked: true},
-  {name: "bread", checked: false}
-]
+  items: [
+    { name: 'apples', checked: false },
+    { name: 'oranges', checked: false },
+    { name: 'milk', checked: true },
+    { name: 'bread', checked: false }
+  ],
+  query: '',
+  checkHidden: false
 };
-
 
 function generateItemElement(item, itemIndex, template) {
   return `
-    <li class="js-item-index-element ${item.checked ? "shopping-item__checked" : ''}" data-item-index="${itemIndex}">
+    <li class="js-item-index-element ${
+      item.checked ? 'shopping-item__checked' : ''
+    }" data-item-index="${itemIndex}">
       <span class="shopping-item js-shopping-item">${item.name}</span>
       
       <div class="shopping-item-controls">
@@ -29,14 +32,21 @@ function generateItemElement(item, itemIndex, template) {
     </li>`;
 }
 
-
 function generateShoppingItemsString(shoppingList) {
-  console.log("Generating shopping list element");
-  const items = shoppingList.map((item, index) => generateItemElement(item, index));
-  return items.join("");
+  console.log('Generating shopping list element');
+
+  let items = shoppingList;
+  if (STORE.query) {
+    items = items.filter(item => item.name.includes(STORE.query));
+  }
+
+  if (STORE.checkHidden) {
+    items = items.filter(item => !item.checked);
+  }
+
+  items = items.map((item, index) => generateItemElement(item, index));
+  return items.join('');
 }
-
-
 
 function renderShoppingList() {
   // render the shopping list in the DOM
@@ -48,10 +58,9 @@ function renderShoppingList() {
   //handleTextEdit();
 }
 
-
 function addItemToShoppingList(itemName) {
   console.log(`Adding "${itemName}" to shopping list`);
-  STORE.items.push({name: itemName, checked: false});
+  STORE.items.push({ name: itemName, checked: false });
 }
 
 function handleNewItemSubmit() {
@@ -66,10 +75,9 @@ function handleNewItemSubmit() {
 }
 
 function toggleCheckedForListItem(itemIndex) {
-  console.log("Toggling checked property for item at index " + itemIndex);
+  console.log('Toggling checked property for item at index ' + itemIndex);
   STORE.items[itemIndex].checked = !STORE.items[itemIndex].checked;
 }
-
 
 function getItemIndexFromElement(item) {
   const itemIndexString = $(item)
@@ -81,6 +89,7 @@ function getItemIndexFromElement(item) {
 function handleItemCheckClicked() {
   $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
     console.log('`handleItemCheckClicked` ran');
+
     const itemIndex = getItemIndexFromElement(event.currentTarget);
     toggleCheckedForListItem(itemIndex);
     renderShoppingList();
@@ -89,10 +98,9 @@ function handleItemCheckClicked() {
 
 // responsible for deleting a list item.
 function deleteListItem(itemIndex) {
-  console.log(`Deleting item at index  ${itemIndex} from shopping list`)
+  console.log(`Deleting item at index  ${itemIndex} from shopping list`);
   STORE.items.splice(itemIndex, 1);
 }
-
 
 function handleDeleteItemClicked() {
   // like in `handleItemCheckClicked`, we use event delegation
@@ -106,56 +114,65 @@ function handleDeleteItemClicked() {
   });
 }
 
-
 function handleCheckBox() {
-    $('#hide-checkbox').on('change', '#js-hide-checkbox', event => {
-    $('.js-shopping-list').find('.shopping-item__checked').toggleClass('hidden');
-    console.log( $('.js-shopping-list').find('.shopping-item__checked'))
-    });
-  }
- 
-//we want STORE.tempItems to hold space for the items that are created from the search function
-// these will then be shown
-  //.val() of these?
+  $('#hide-checkbox').on('change', '#js-hide-checkbox', event => {
+    /* $('.js-shopping-list')
+      .find('.shopping-item__checked')
+      .toggleClass('hidden');*/
+    //debugger;
 
-  function filterSearchItem(input) {
-    STORE.items = STORE.items.filter(item => item.name.includes(input));
-    // Logic to update the list
-  }
+    let setting = $(event.currentTarget).prop('checked');
+    setHideChecked(setting);
+    renderShoppingList();
 
-function handleSearchItem(){
-$('.topnav').on("click", "button", event =>{
-    console.log(`handleSearchItem is running`)
-    event.preventDefault();
-    const searchList =$('.js-search-list').val();
+    console.log($('.js-shopping-list').find('.shopping-item__checked'));
+  });
+}
+
+function setHideChecked(input) {
+  if (input === true) {
+    STORE.checkHidden = true;
+  } else {
+    STORE.checkHidden = false;
+  }
+}
+
+
+function filterSearchItem(input) {
+  //debugger;
+  STORE.query = input;
+  // Logic to update the list
+}
+
+function handleSearchItem() {
+  $('.topnav').on('click', 'button', event => {
+   // debugger;
+    console.log(`handleSearchItem is running`);
+    const searchList = $('.js-search-list').val();
     filterSearchItem(searchList);
-})      
- renderShoppingList();   
-};
+    renderShoppingList();
+  });
+}
 
-
-function EditItemList(itemIndex, word){
-    STOREitems.splice(itemIndex, 1, word);
+function EditItemList(itemIndex, word) {
+  STORE.items.splice(itemIndex, 1, word);
 }
 
 function handleTextEdit() {
- // this function will allow users to edit the existing listed items   
- //listen to the edit button, and run the function  for the li  
- $('.js-item-index-element').on('click', '.js-item-edit', (function() {
+  // this function will allow users to edit the existing listed items
+  //listen to the edit button, and run the function  for the li
+  $('.js-item-index-element').on('click', '.js-item-edit', function() {
     console.log('handleTextEdit is running');
-  //make sure page doesn't stay in default  
+    //make sure page doesn't stay in default
     event.preventDefault();
-// declare item index is the item that is being click on from getItemIndexFromElement func
-    let itemIndex= getItemIndexFromElement(event.target);
- // capture the new edited item to the input
- //let editedItem =    
- }));
+    // declare item index is the item that is being click on from getItemIndexFromElement func
+    let itemIndex = getItemIndexFromElement(event.target);
+    // capture the new edited item to the input
+    //let editedItem = generateItemElement(input) 
 
-};
-
-
-
-
+    renderShoppingList();
+  });
+}
 
 function handleShoppingList() {
   renderShoppingList();
